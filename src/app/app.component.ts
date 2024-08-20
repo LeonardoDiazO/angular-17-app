f30228b50694d2e474f4594c41242875895d1263
LeonardoDiazO <maximun_neo@hotmail.com>
2024-08-19 20:05:57 -0500

    Refactor components amd add new comments feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserComponent],
+  imports: [CommonModule, RouterOutlet, UserComponent, CommentsComponent],
   template: `
     <div>
       <h1>{{ title }}</h1>
@@ -17,6 +18,7 @@ import { UserComponent } from './user/user.component';
       <app-user></app-user>
     </div>
   `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <ul>
     @for(game of games; track game.id){
-        <li>{{game.name}}</li>
+        <li (click)="fav(game.name)">{{game.name}}</li>
     }
     </ul>
   `,
   styles: ``
 })
 export class GamesComponent {
+  @Input() username = ''; // <-- Decorador @Input
+  @Output() addFavoriteEvent = new EventEmitter<string>();
+
+  fav(gameName: string){
+    this.addFavoriteEvent.emit(gameName);
+  }
 
   games = [
     {
       id: 1,
-      name : 'Uncharted 4'
+      name: 'Uncharted 4'
     },
     {
       id: 2,
-      name: 'Horizon zero dawm'
+      name: 'Horizon Zero Dawn'
     },
     {
       id: 3,
-      name: 'BloodMan'
+      name: 'Bloodborne'
     }
-  ]
+  ];
 
+  trackById(index: number, game: { id: number }) {
+    return game.id;
+  }
 }
